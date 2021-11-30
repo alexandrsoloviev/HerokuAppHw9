c1447c615f19dd849635ecd8095a0bdebc3e84e2
alexandr <a.soloviev@icol.com>
2021-11-30 22:04:13 +0300

    Initial commit

---
+++ src/test/java/pages/ContextMenuPage.java
@@ -0,0 +1,10 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class ContextMenuPage extends BasePage{
+
+    public ContextMenuPage(WebDriver driver) {
+        super(driver);
+    }
+}
